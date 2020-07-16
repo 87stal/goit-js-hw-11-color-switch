4895692c734a7a93e78cb5cc485f95689eedbe7e
87stal <87stal@gmail.com>
2020-07-16 16:35:31 +0300

    First exercise done

---
+++ src/js/color-switch.js
@@ -0,0 +1,41 @@
+import colors from './colors';
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+let timerId = null;
+let isActive = false;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function onStartChange(items) {
+if(isActive){
+   refs.startBtn.disabled = true; 
+}
+  const currentColor = randomIntegerFromInterval(0, this.length);
+  console.log(currentColor);
+  refs.body.style.backgroundColor = this[currentColor];
+  isActive = true;
+};
+
+refs.startBtn.addEventListener('click', () => {
+    timerId = setInterval(onStartChange.bind(colors), 1000)   
+});
+
+refs.stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    isActive = false;
+    refs.startBtn.disabled = false;
+  });
+
+
+
+  
+
+
+
